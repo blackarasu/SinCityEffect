d53c6bfac52fce1d613a3b0d9e9fd4a828bbdb4a
Blackarasu <bartek2912@op.pl>
2018-12-11 22:09:22 +0100

    Added Calibration

--- DLL_CPP/DLL_CPP.h
+++ DLL_CPP/DLL_CPP.h
@@ -2,9 +2,8 @@
 #include "stdafx.h"
 #include "../mainProgram/Constatnts.h"
 //void RgbToHsl(); //todo parameters -> zamiana rgb na hsl
-
-//void HslToRgb(); //todo parameters -> zamiana rgb na hsl
 const int RANGE = 14;
+//void HslToRgb(); //todo parameters -> zamiana rgb na hsl
 void ProcedeSCPP(int *hues, float *saturations, int H, int ammountToCalculate); //todo parameters -> operacja na HSL
 //parametry:
 //hue ->szukane hue do zwiêkszenia saturacji b¹dz jej ustawieniu na 0 
